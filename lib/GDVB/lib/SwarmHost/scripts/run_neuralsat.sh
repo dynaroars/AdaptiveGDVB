0db256d9d69ca0de16d55aea893e9e4438cfeb79
Nguyen <hodangkhoinguyen@gmail.com>
2025-04-25 16:56:52 -0400

    test with neuralsat

--- lib/GDVB/lib/SwarmHost/scripts/run_neuralsat.sh
+++ lib/GDVB/lib/SwarmHost/scripts/run_neuralsat.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-. ${SwarmHost}/scripts/init_conda.sh
-#conda activate neuralsat
-conda activate neuralsat2
+# . ${SwarmHost}/scripts/init_conda.sh
+# #conda activate neuralsat
+# conda activate neuralsat2
 
-which python
-export MKL_SERVICE_FORCE_INTEL=1
+# which python
+# export MKL_SERVICE_FORCE_INTEL=1
 
 #cmd="python $SwarmHost/lib/neuralsat/src/main.py $@"
 #cmd="python $SwarmHost/lib/neuralsat/neuralsat/main.py $@"
@@ -13,4 +13,4 @@ cmd="python $SwarmHost/lib/neuralsat/neuralsat-pt201/main.py $@"
 echo $cmd
 $cmd
 
-conda deactivate
+# conda deactivate
